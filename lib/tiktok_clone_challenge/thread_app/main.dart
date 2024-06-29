8b7bb23ba6599f9cceb4aef2db226c07cfa1e5eb
Chamsol Kim <kcsol1005@gmail.com>
2024-06-29 14:11:38 +0900

    thread clone: configure firebase, add firebase_auth plugin

--- lib/tiktok_clone_challenge/thread_app/main.dart
+++ lib/tiktok_clone_challenge/thread_app/main.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nomadcoders_flutter_challenge/firebase_options.dart';
 import 'package:nomadcoders_flutter_challenge/tiktok_clone_challenge/thread_app/router.dart';
 import 'package:nomadcoders_flutter_challenge/tiktok_clone_challenge/thread_app/services/theme_mode_service.dart';
 import 'package:nomadcoders_flutter_challenge/tiktok_clone_challenge/thread_app/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   if (kIsWeb) {
     GoRouter.optionURLReflectsImperativeAPIs = true;
   }
