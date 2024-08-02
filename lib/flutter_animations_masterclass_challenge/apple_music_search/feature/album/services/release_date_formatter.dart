d30a9b20ba39289dee25c934a9182a930cdee54a
Chamsol Kim <kcsol1005@gmail.com>
2024-08-02 20:16:01 +0900

    apple music search 졸업과제를 별도 repository로 관리하도록 유지 (rollback)

--- lib/flutter_animations_masterclass_challenge/apple_music_search/feature/album/services/release_date_formatter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-class ReleaseDateFormatter {
-  final _dateFormat = DateFormat("yyyy-MM-ddTHH:mm:ssZ");
-
-  DateTime dateTime(String dateString) => _dateFormat.parse(dateString);
-}
--- lib/flutter_animations_masterclass_challenge/apple_music_search/theme/light_theme_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-final lightThemeData = ThemeData(
-  scaffoldBackgroundColor: Colors.white,
-  primaryColor: const Color(0xFFFA233B),
-  dividerColor: Colors.grey.shade300,
-);
